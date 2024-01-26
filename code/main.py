e86c53d1b6fde467440f162fdead9151e000dc9e
shuminchang <magic2313@gmail.com>
2024-01-27 01:57:16 +0800

    level setup, re organize code

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game:
     def __init__(self):
@@ -7,6 +8,8 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Zelda')
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
@@ -16,6 +19,7 @@ class Game:
                     sys.exit()
             
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
