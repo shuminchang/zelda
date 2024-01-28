bf70702870b60cd4e9065708f6da9e77e5537772
shuminchang <magic2313@gmail.com>
2024-01-29 00:11:24 +0800

    add user interface

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ from debug import debug
 from support import *
 from random import choice
 from weapon import Weapon
+from ui import UI
 
 class Level:
     def __init__(self):
@@ -22,6 +23,9 @@ class Level:
 
         # sprite group setup
         self.create_map()
+
+        # user interface
+        self.ui = UI()
 
     def create_map(self):
         layouts = {
@@ -62,7 +66,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.status)
+        self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
--- code/settings.py
+++ code/settings.py
@@ -1,12 +1,31 @@
 import os
 
+dir_path = os.getcwd()
+
 # game setup
 WIDTH    = 1280	
 HEIGHT   = 720
 FPS      = 60
 TILESIZE = 64
 
-dir_path = os.getcwd()
+# ui
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = os.path.join(dir_path, 'graphics\\font\\joystix.ttf')
+UI_FONT_SIZE = 18
+
+# general colors
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+
+# ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
 
 # weapons 
 weapon_data = {
