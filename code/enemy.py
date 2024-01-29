47befc78f404d80b6d13cff160bb86364c1411da
shuminchang <magic2313@gmail.com>
2024-01-29 23:51:09 +0800

    add upgrade system and menu

--- code/enemy.py
+++ code/enemy.py
@@ -4,7 +4,7 @@ from entity import Entity
 from support import *
 
 class Enemy(Entity):
-    def __init__(self, monster_name, pos, groups, obstacle_sprites, damage_player, trigger_death_particles):
+    def __init__(self, monster_name, pos, groups, obstacle_sprites, damage_player, trigger_death_particles, add_exp):
         
         # general setup
         super().__init__(groups)
@@ -38,6 +38,7 @@ class Enemy(Entity):
         self.attack_cooldown = 400
         self.damage_player = damage_player
         self.trigger_death_particles = trigger_death_particles
+        self.add_exp = add_exp
 
         # invincibility timer
         self.vulnerable = True
@@ -118,7 +119,7 @@ class Enemy(Entity):
                 self.health -= player.get_full_weapon_damage()
             else:
                 self.health -= player.get_full_magic_damage()
-                
+
             self.hit_time = pygame.time.get_ticks()
             self.vulnerable = False
         
@@ -126,6 +127,7 @@ class Enemy(Entity):
         if self.health <= 0:
             self.kill()
             self.trigger_death_particles(self.rect.center, self.monster_name)
+            self.add_exp(self.exp)
 
     def hit_reaction(self):
         if not self.vulnerable:
